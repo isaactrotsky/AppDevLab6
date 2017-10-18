caa6077971940cc80f5521055544de77d8a9ce6a
Local Account 436-01 <436-01@csc302x34.csc.calpoly.edu>
2017-10-18 10:10:44 -0700

    Initial Commit

---
+++ Lab6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab6
+//
+//  Created by Local Account 436-01 on 10/18/17.
+//  Copyright © 2017 Local Account 436-01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
